fda7ef6bdd414bf1bf606ef938a54addda4ddda0
triest <triest21@gmail.com>
2025-05-21 23:41:32 +0300

    final commit

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -1,5 +1,3 @@
-<!-- resources/views/catalog/index.blade.php -->
-
 @extends('layouts.app')
 
 @section('content')
@@ -14,14 +12,23 @@
                             <h5 class="card-title">{{ $product->name }}</h5>
                             <p class="card-text">{{ $product->description }}</p>
                             <p class="text-muted">Price: ${{ number_format($product->price, 2) }}</p>
-                            <form action="{{ route('order.add-item') }}" method="POST">
+
+                            <!-- Форма с явным указанием области действия -->
+                            <form action="{{ route('order.add-item') }}" method="POST" class="d-inline" >
                                 @csrf
                                 <input type="hidden" name="product_id" value="{{ $product->id }}">
-                                <div class="input-group mb-3">
-                                    <input type="number" name="quantity" value="1" min="1" max="{{ $product->quantity }}"
-                                           class="form-control" required>
-                                    <button type="submit" class="btn btn-primary">Add to Order</button>
+                                <div class="input-group">
+                                    <input type="number"
+                                           name="quantity"
+                                           value="1"
+                                           min="1"
+                                           max="{{ $product->quantity }}"
+                                           class="form-control"
+                                           required
+                                           style="max-width: 80px">
+
                                 </div>
+                                <input type="submit">
                             </form>
                         </div>
                     </div>
